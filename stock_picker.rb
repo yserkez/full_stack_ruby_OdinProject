db49a6b879d4bb1466b7e367981a32236328dff0
Yosef <yosefserkez@gmail.com>
2021-02-04 14:17:16 -0500

    comment

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,3 +1,12 @@
+=begin
+    Implement a method #stock_picker that takes in an array of stock prices, one for each hypothetical day.
+    It should return a pair of days representing the best day to buy and the best day to sell. Days start at 0.
+
+    EXAMPLE:
+    > stock_picker([17,3,6,9,15,8,6,1,10])
+    => [1,4]  # for a profit of $15 - $3 == $12
+
+=end
 def stock_picker (days)
     max_gain = 0
     best_days = [0,0]
