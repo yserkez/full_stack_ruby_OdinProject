4e3a0cede92b81e810281f2bfe41b8ea0cf78b89
Yosef <yosefserkez@gmail.com>
2021-02-04 13:03:57 -0500

    caeser-cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,4 +1,12 @@
-#!/usr/bin/env ruby
+=begin
+
+INSTRUCTIONS:
+  Implement a caesar cipher that takes in a string and the shift factor and then outputs the modified string:
+EXAMPLE:
+  caesar_cipher("What a string!", 5) => "Bmfy f xywnsl!"
+
+=end
+  
 def encode (character, key)
   char_code = character.downcase.ord #downcase and get ascii value of character
   return char_code unless (char_code >= 97 && char_code <= 122) #return all non alphabetic characters as is
@@ -7,21 +15,15 @@ def encode (character, key)
   new_val = char_code + key
   
   new_val += 26 if (new_val < 97) #if new value is outside of alphabet its should wrap back around
-  new_val -= 26 if (new_val > 122) #if new value is outside of alphabet its should wrap back around
+  new_val -= 26 if (new_val > 122)
   new_val -= 32 unless character.eql?(character.downcase) #if character was originally uppercase convert it back
   return new_val
 end
 
-def cipher (message,key)
-  message.chars.map{|ch| encode(ch,key)}.pack('c*')
-end
-
-if (ARGV[0] && ARGV[1]) 
-  unless ARGV[1].to_i
-    puts "Second argument must be a number"
+def caesar_cipher (message,key)
+  unless message.is_a?(String) && key.is_a?(Numeric)
+    puts "invalid arguments"
     return
   end
-  puts cipher(ARGV[0], ARGV[1].to_i)
- else
-  puts "Two arguments required"""
+  message.chars.map{|ch| encode(ch,key)}.pack('c*') #rotate each character and then return to string
 end
